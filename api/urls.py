88de49ae69e4fd5924d2d8b74dade10912231172
ibadullaevn <40333337+ibadullaevn@users.noreply.github.com>
2020-04-26 02:58:04 +0600

    auth_jwt

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from api.views import CategoryListAPIView, CategoryDetailsAPIView, ProductListAPIView, ProductDetailsAPIView
-
+from rest_framework_jwt.views import obtain_jwt_token
 urlpatterns = [
     # path('categories/', category_list),
     # path('categories/<int:category_id>/', category_details),  Function Based
     # path('products/', product_list),
     # path('products/<int:product_id>/', product_details),
-
+    path('login/', obtain_jwt_token),
     path('categories/', CategoryListAPIView.as_view()),
     path('categories/<int:category_id>/', CategoryDetailsAPIView.as_view()),
     path('products/', ProductListAPIView.as_view()),
